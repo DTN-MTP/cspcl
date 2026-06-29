7d331c6217ec1a876183807c0e8ea087bf883f3c
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2026-06-29 23:12:42 +0200

    feat(rust): add tracing subscriber for cspcl-server
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- rust-bindings/hardy-cspcl-server/src/main.rs
+++ rust-bindings/hardy-cspcl-server/src/main.rs
@@ -10,9 +10,14 @@ use crate::{
     error::ServerError::{self},
 };
 use clap::Parser;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> Result<(), ServerError> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     let config = config::Config::parse();
     let server_config = config::load_server_config(&config.config_path)?;
     let cla = Arc::new(create_cla(server_config.cspcl_config)?);
