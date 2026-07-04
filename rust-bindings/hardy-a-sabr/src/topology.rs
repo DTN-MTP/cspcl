db842161f6fe9a87496f07e49507b05e4da1bad4
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2026-07-04 18:01:49 +0200

    feat(rust): map a-sabr node with hardy nodes for topology
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- rust-bindings/hardy-a-sabr/src/topology.rs
+++ rust-bindings/hardy-a-sabr/src/topology.rs
@@ -1,3 +1,5 @@
+use hardy_bpv7::eid::{Eid, NodeId};
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct ContactWindow {
     pub tx_node_id: u16,
@@ -8,12 +10,26 @@ pub struct ContactWindow {
     pub delay: f64,
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub struct NodeMapping {
+    pub asabr_node_id: u16,
+    pub hardy_node_id: NodeId,
+}
+
 #[derive(Debug, Clone, Default, PartialEq)]
 pub struct TopologySnapshot {
+    pub nodes: Vec<NodeMapping>,
     pub contacts: Vec<ContactWindow>,
 }
 
 impl TopologySnapshot {
+    pub fn hardy_eid_for(&self, asabr_node_id: u16) -> Option<Eid> {
+        self.nodes
+            .iter()
+            .find(|node| node.asabr_node_id == asabr_node_id)
+            .map(|node| node.hardy_node_id.clone().into())
+    }
+
     pub fn next_boundary_after(&self, now: f64) -> Option<f64> {
         self.contacts
             .iter()
