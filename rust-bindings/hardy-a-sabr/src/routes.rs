e200d483692da402664ea05a9f7858b72d4480c0
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2026-07-04 18:36:53 +0200

    feat(rust): apply route diff to sink & reconcile with installed route
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- rust-bindings/hardy-a-sabr/src/routes.rs
+++ rust-bindings/hardy-a-sabr/src/routes.rs
@@ -1,4 +1,4 @@
-use hardy_bpa::routing::RouteAction;
+use hardy_bpa::routing::{self, RouteAction, RoutingSink};
 use hardy_eid_patterns::EidPattern;
 
 #[derive(Debug, Clone, PartialEq)]
@@ -28,4 +28,34 @@ pub fn diff_routes(installed: &[ProjectedRoute], desired: &[ProjectedRoute]) -> 
         .collect();
 
     RouteDiff { remove, add }
+}
+
+pub async fn apply_route_diff(
+    sink: &dyn RoutingSink,
+    diff: &RouteDiff,
+) -> routing::Result<Vec<ProjectedRoute>> {
+    let mut accepted = Vec::new();
+
+    for route in &diff.remove {
+        sink.remove_route(&route.pattern, &route.action, route.priority)
+            .await?;
+    }
+
+    for route in &diff.add {
+        sink.add_route(route.pattern.clone(), route.action.clone(), route.priority)
+            .await?;
+
+        accepted.push(route.clone());
+    }
+
+    Ok(accepted)
+}
+
+pub fn reconcile_installed_routes(
+    installed: &mut Vec<ProjectedRoute>,
+    diff: &RouteDiff,
+    accepted: Vec<ProjectedRoute>,
+) {
+    installed.retain(|route| !diff.remove.iter().any(|removed| removed == route));
+    installed.extend(accepted);
 }
